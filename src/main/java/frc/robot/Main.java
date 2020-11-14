667174871502e6e3cf641bc0ca74bb14e2608b9e
Alexander McDowell <lextmcdowell@gmail.com>
2020-11-13 17:10:27 -0800

    Remove jnilib loading from Main

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -7,11 +7,6 @@
 
 package frc.robot;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
-
 import edu.wpi.first.wpilibj.RobotBase;
 
 /**
@@ -29,25 +24,6 @@ public final class Main {
    * <p>If you change your main robot class, change the parameter type.
    */
   public static void main(String... args) {
-    // Move the jnilib files manually to tmp/jniExtractDir since it didn't happen automatically
-    if (System.getProperty("os.name").equals("Mac OS X")) {
-      String dir = System.getProperty("user.dir");
-      String files[] = {"libJavaController.jnilib", "libvehicle.jnilib"};
-      Path src, dest;
-      for (String s : files) {
-        System.load(dir + "/libs/jnilibs/" + s);
-        try {
-          src = Path.of(dir + "/libs/jnilibs/" + s);
-          dest = Path.of(dir + "/build/tmp/jniExtractDir/" + s);
-          if (!Files.exists(dest)) Files.createFile(dest);
-          Files.copy(src, dest, StandardCopyOption.REPLACE_EXISTING);
-        } catch (IOException io) {
-          io.printStackTrace();
-          System.exit(1);
-        }
-      }
-    }
-    
     RobotBase.startRobot(Robot::new);
   }
 }
