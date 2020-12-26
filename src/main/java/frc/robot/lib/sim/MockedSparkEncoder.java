448ecf7437ef81609c047380f3436d7a54e4c833
CoolSpy3 <55305038+CoolSpy3@users.noreply.github.com>
2020-12-26 14:59:36 -0800

    revert previous changes and switch all sparks and encoders to SimDevices

--- src/main/java/frc/robot/lib/sim/MockedSparkEncoder.java
+++ src/main/java/frc/robot/lib/sim/MockedSparkEncoder.java
@@ -4,49 +4,48 @@ import com.revrobotics.CANEncoder;
 import com.revrobotics.CANError;
 import com.revrobotics.CANSparkMax;
 
-import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.simulation.EncoderSim;
+import edu.wpi.first.hal.SimDevice;
+import edu.wpi.first.hal.SimDouble;
 
 public class MockedSparkEncoder extends CANEncoder {
-    private Encoder encoder;
-    private EncoderSim encoderSim;
+    private SimDevice device;
+    private SimDouble dpp;
+    private SimDouble count;
     // Default value for a CANEncoder
     private final int countsPerRevolution = 4096;
-    // Used for linking EncoderSim with Encoder
-    private static int encodersCreated = 0;
 
     public MockedSparkEncoder(CANSparkMax motor) {
         super(motor);
         int id = motor.getDeviceId();
         // Match motor on CAN 0 with channels [0, 1], CAN 1 to channels [2, 3], etc.
         // Probably not the best way to do it but it works
-        encoder = new Encoder(2 * id, 2 * id + 1);
-        encoderSim = EncoderSim.createForIndex(encodersCreated);
-        encodersCreated++;
+        device = SimDevice.create("EncoderSim_SparkMax", id);
+        dpp = device.createDouble("distancePerPulse", false, 1);
+        count = device.createDouble("count", false, 0);
     }
 
     @Override
     public double getPosition() {
-        return encoder.getDistance();
+        return dpp.get() * count.get();
     }
 
     @Override
     public CANError setPositionConversionFactor(double positionConversionFactor) {
         // Assume positionConversionFactor = units/rev
         // distancePerPulse (actually distance per count) = units/rev * rev/count
-        encoder.setDistancePerPulse(positionConversionFactor / countsPerRevolution);
+        dpp.set(positionConversionFactor / countsPerRevolution);
         return CANError.kOk;
     }
 
     @Override
     public CANError setPosition(double position) {
         double revolutions = position / getPositionConversionFactor();
-        encoderSim.setCount((int) Math.floor(revolutions * countsPerRevolution));
+        count.set((int) Math.floor(revolutions * countsPerRevolution));
         return CANError.kOk;
     }
 
     @Override
     public double getPositionConversionFactor() {
-        return encoder.getDistancePerPulse() * countsPerRevolution;
+        return dpp.get() * countsPerRevolution;
     }
 }
