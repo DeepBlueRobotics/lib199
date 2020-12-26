a1735db5ab00b4554676b7618d708e3beb1ed53e
CoolSpy3 <55305038+CoolSpy3@users.noreply.github.com>
2020-12-26 11:30:57 -0800

    fix #4

--- src/main/java/frc/robot/lib/sim/MockSparkMax.java
+++ src/main/java/frc/robot/lib/sim/MockSparkMax.java
@@ -20,7 +20,8 @@ public class MockSparkMax extends CANSparkMax {
     private static HashMap<Integer, Spark> followMap = new HashMap<Integer, Spark>();
 
     public MockSparkMax(int portPWM, MotorType type) {
-        super(portPWM, type);
+        super(portPWM+10, type);
+        portPWM += 10;
         this.portPWM = portPWM;
         motorPWM = new Spark(portPWM);
         encoder = Mocks.createMock(CANEncoder.class, new MockedSparkEncoder(this));
