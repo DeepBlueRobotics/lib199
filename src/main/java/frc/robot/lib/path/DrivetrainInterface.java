37886677c3cd9c8b1069d4da5e2cd917392574c8
0sani <jackahansen18@gmail.com>
2021-10-18 16:13:22 -0700

    Added units and javadocs

--- src/main/java/frc/robot/lib/path/DrivetrainInterface.java
+++ src/main/java/frc/robot/lib/path/DrivetrainInterface.java
@@ -11,18 +11,48 @@ import edu.wpi.first.wpilibj2.command.Subsystem;
 
 public interface DrivetrainInterface extends Subsystem {
 
+    /**
+     * Configures the constants for generating a trajectory
+     * @param config The configuration for generating a trajectory
+     */
     public void configureTrajectory(TrajectoryConfig config);
 
+    /**
+     * Constructs a new RamseteCommand that, when executed, will follow the provided trajectory.
+     * @param trajectory The trajectory to follow.
+     * @param desiredHeading A function that supplies the robot pose - use one of the odometry classes to provide this.
+     * @return RamseteCommand
+     */
     public Command createRamseteCommand(Trajectory trajectory, Supplier<Rotation2d> desiredHeading);
 
+    /**
+     * Gets the Autonomous max acceleration in m/s^2
+     * @return max acceleration in m/s^2
+     */
     public double getAutoMaxAccel();
 
+    /**
+     * Gets the Autonomous max speed in m/s
+     * @return max speed in m/s
+     */
     public double getAutoMaxSpeed();
 
+    /**
+     * Gets the current heading in degrees
+     * @return current heading in degrees
+     */
     public double getHeading();
 
+    /**
+     * Sets odometry based on current gyro angle and pose
+     * @param gyroAngle The angle reported by the gyroscope.
+     * @param initialPose The starting position of the robot on the field.
+     */
     public void setOdometry(Rotation2d gyroAngle, Pose2d initialPose);
 
+    /**
+     * Stops the drivetrain
+     */
     public void stop();
 
 }
