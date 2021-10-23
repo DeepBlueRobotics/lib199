6d8de3fb2fa4caee0588a0ea5623243a840d25ce
CoolSpy3 <coolspythree@gmail.com>
2021-10-23 16:45:56 -0700

    rename loadOdometry to initializeDrivetrainPosition

--- src/main/java/frc/robot/lib/path/RobotPath.java
+++ src/main/java/frc/robot/lib/path/RobotPath.java
@@ -84,7 +84,7 @@ public class RobotPath {
         // the current heading
         Rotation2d heading = Rotation2d.fromDegrees(dt.getHeadingDeg());
         Supplier<Rotation2d> desiredHeading = (!faceInPathDirection) ? () -> heading : () -> hs.sample();
-        Command command = new InstantCommand(this::loadOdometry)
+        Command command = new InstantCommand(this::initializeDrivetrainPosition)
                 .andThen(dt.createAutoCommand(trajectory, desiredHeading));
         if (stopAtEnd) {
             command = command.andThen(new InstantCommand(dt::stop, dt));
@@ -93,9 +93,13 @@ public class RobotPath {
     }
 
     /**
-     * Loads odometry
+     * Assumes that the robot is at the starting position of this path and calls
+     * {@link DrivetrainInterface#setOdometry(Rotation2d, Pose2d)} with the
+     * parameters to initialize an odometry for that state. It is up to the
+     * {@link DrivetrainInterface}'s implementation to determine how to interpret
+     * this call.
      */
-    public void loadOdometry() {
+    public void initializeDrivetrainPosition() {
         if (trajectory == null) {
             generateTrajectory();
         }
