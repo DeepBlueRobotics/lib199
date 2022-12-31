59c2ebdfb3b404404f95e1f421fde7bf1caed808
CoolSpy3 <coolspythree@gmail.com>
2022-12-31 13:36:52 -0800

    remove cancoder motor information

--- src/main/java/org/carlmontrobotics/lib199/sim/MockedCANCoder.java
+++ src/main/java/org/carlmontrobotics/lib199/sim/MockedCANCoder.java
@@ -20,8 +20,6 @@ public class MockedCANCoder {
     private int port;
     private SimDevice device;
     private SimDouble position; // Rotations - Continuous
-    private SimDouble motorId;
-    private SimDouble motorType;
     private SimDouble gearing;
     private CANCoderSimCollection sim;
 
@@ -29,8 +27,6 @@ public class MockedCANCoder {
         port = canCoder.getDeviceID();
         device = SimDevice.create("CANCoder", port);
         position = device.createDouble("count", Direction.kInput, 0);
-        motorId = device.createDouble("motorId", Direction.kOutput, -1);
-        motorType = device.createDouble("motorType", Direction.kOutput, MotorType.NONE.id);
         gearing = device.createDouble("gearing", Direction.kOutput, 1);
         sim = canCoder.getSimCollection();
         Lib199Subsystem.registerPeriodic(this::update);
@@ -41,24 +37,12 @@ public class MockedCANCoder {
         sim.setRawPosition((int) (position.get() * kCANCoderCPR));
     }
 
-    public void linkMotor(int motorId, MotorType motorType, double gearing) {
-        this.motorId.set(motorId);
-        this.motorType.set(motorType.id);
+    public void setGearing(double gearing) {
         this.gearing.set(gearing);
     }
 
-    public static void linkMotor(int canCoderPort, int motorId, MotorType motorType, double gearing) {
-        if(sims.containsKey(canCoderPort)) sims.get(canCoderPort).linkMotor(motorId, motorType, gearing);
-    }
-
-    public static enum MotorType {
-        NONE(0), TALON(1), VICTOR(2), SPARKMAX(3);
-
-        public final int id;
-
-        private MotorType(int id) {
-            this.id = id;
-        }
+    public static void setGearing(int port, double gearing) {
+        if(sims.containsKey(port)) sims.get(port).setGearing(gearing);
     }
 
 }
