1b503cc4c360c3776b9251fcd8e2c230a5c544dd
CoolSpy3 <coolspythree@gmail.com>
2023-05-21 00:26:57 -0700

    deprecate CachedSparkMax

--- src/main/java/org/carlmontrobotics/lib199/MotorControllerFactory.java
+++ src/main/java/org/carlmontrobotics/lib199/MotorControllerFactory.java
@@ -120,7 +120,7 @@ public class MotorControllerFactory {
   public static CANSparkMax createSparkMax(int id, MotorConfig config) {
     CANSparkMax spark;
     if (RobotBase.isReal()) {
-      spark = new CachedSparkMax(id, CANSparkMaxLowLevel.MotorType.kBrushless);
+      spark = new CANSparkMax(id, CANSparkMaxLowLevel.MotorType.kBrushless);
       if (spark.getFirmwareVersion() == 0) {
         spark.close();
         System.err.println("SparkMax on port: " + id + " is not connected!");
