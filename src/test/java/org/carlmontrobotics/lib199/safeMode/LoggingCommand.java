73cb8efcfbb0f0bfbda0f1262262e023b03cb35b
CoolSpy3 <coolspythree@gmail.com>
2023-06-29 13:50:05 -0700

    document LoggingCommand

--- src/test/java/org/carlmontrobotics/lib199/safeMode/LoggingCommand.java
+++ src/test/java/org/carlmontrobotics/lib199/safeMode/LoggingCommand.java
@@ -2,6 +2,11 @@ package org.carlmontrobotics.lib199.safeMode;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
+/**
+ * A command that counts how many times it has been initialized and executed for the purposes of testing safe mode functionality.
+ *
+ * @see SafeModeCommandsTest
+ */
 public class LoggingCommand extends CommandBase {
 
     private int initializedCount = 0, executeCount = 0;
@@ -16,14 +21,23 @@ public class LoggingCommand extends CommandBase {
         executeCount++;
     }
 
+    /**
+     * Resets the initialized and execute counts to zero.
+     */
     public void reset() {
         initializedCount = executeCount = 0;
     }
 
+    /**
+     * @return The number of times this command has been initialized.
+     */
     public int getInitializedCount() {
         return initializedCount;
     }
 
+    /**
+     * @return The number of times this command has been executed.
+     */
     public int getExecuteCount() {
         return executeCount;
     }
