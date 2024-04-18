5e38df18cbb0e85c1cd088df3b92c46f5537bf05
Dean Brettle <dean@brettle.com>
2024-04-18 16:27:02 -0700

    Start adding plumbing to enable sim of SparkPIDController.

--- src/main/java/org/carlmontrobotics/lib199/sim/MockedSparkMaxPIDController.java
+++ src/main/java/org/carlmontrobotics/lib199/sim/MockedSparkMaxPIDController.java
@@ -1,14 +1,59 @@
 package org.carlmontrobotics.lib199.sim;
 
-import com.revrobotics.REVLibError;
+import java.util.Arrays;
 
+import com.revrobotics.CANSparkBase;
+import com.revrobotics.REVLibError;
+import com.revrobotics.SparkPIDController;
+import com.revrobotics.CANSparkBase.ControlType;
+import com.revrobotics.SparkPIDController.ArbFFUnits;
+
+import edu.wpi.first.hal.SimDevice;
+import edu.wpi.first.hal.SimDouble;
+import edu.wpi.first.hal.SimEnum;
+import edu.wpi.first.hal.SimDevice.Direction;
 import edu.wpi.first.math.controller.PIDController;
 
 public class MockedSparkMaxPIDController {
     private PIDController pidController;
+    private int portNumber;
+    SimDevice pidControllerSim;
+    SimDouble velocitySim;
+    SimEnum controlTypeSim;
+    SimDouble arbFFSim;
+    SimEnum arbFFUnitsSim;
 
-    public MockedSparkMaxPIDController() {
+    public MockedSparkMaxPIDController(int portNumber) {
+        this.portNumber = portNumber;
         pidController = new PIDController(0.0, 0.0, 0.0);
+        pidControllerSim = SimDevice.create("SparkPIDController", portNumber);
+
+        velocitySim = pidControllerSim.createDouble("velocity", Direction.kBidir, 0);
+
+        String[] controlTypeNames = Arrays.stream(ControlType.values()).map(ControlType::name).toArray(String[]::new);
+        controlTypeSim = pidControllerSim.createEnum("controlType", Direction.kBidir, controlTypeNames, ControlType.kDutyCycle.ordinal());
+
+        arbFFSim = pidControllerSim.createDouble("arbFF", Direction.kBidir, 0);
+
+        String[] arbFFUnitNames = Arrays.stream(ArbFFUnits.values()).map(ArbFFUnits::name).toArray(String[]::new);
+        arbFFUnitsSim = pidControllerSim.createEnum("arbFFUnits", Direction.kBidir, arbFFUnitNames, ArbFFUnits.kVoltage.ordinal());
+    }
+
+    @Deprecated
+    public MockedSparkMaxPIDController() {
+        this(0);
+    }
+
+    public REVLibError setReference​(double value, CANSparkBase.ControlType ctrl, int pidSlot, double arbFeedforward, SparkPIDController.ArbFFUnits arbFFUnits) {
+        controlTypeSim.set(ctrl.ordinal());
+        arbFFSim.set(arbFeedforward);
+        arbFFUnitsSim.set(arbFFUnits.ordinal());
+        switch (ctrl) {
+            case kVelocity:
+                velocitySim.set(value);
+                break;
+        }
+        return REVLibError.kOk;
     }
 
     public REVLibError setP(double gain) {
--- src/test/java/org/carlmontrobotics/lib199/sim/MockedSparkMaxPIDControllerTest.java
+++ src/test/java/org/carlmontrobotics/lib199/sim/MockedSparkMaxPIDControllerTest.java
@@ -1,26 +1,64 @@
 package org.carlmontrobotics.lib199.sim;
 
 import static org.junit.Assert.assertEquals;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.CoreMatchers.*;
 
 import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
+import com.revrobotics.CANPIDController;
 import com.revrobotics.REVLibError;
+import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.SparkPIDController;
+import com.revrobotics.CANSparkBase.ControlType;
+import com.revrobotics.SparkPIDController.ArbFFUnits;
+
+import edu.wpi.first.hal.SimDouble;
+import edu.wpi.first.hal.SimEnum;
+import edu.wpi.first.wpilibj.simulation.SimDeviceSim;
 
 import org.carlmontrobotics.lib199.Mocks;
 import org.carlmontrobotics.lib199.REVLibErrorAnswer;
+import org.carlmontrobotics.lib199.testUtils.TestRules;
+import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
 
 public class MockedSparkMaxPIDControllerTest {
-    
+
+    @ClassRule
+    public static TestRules.InitializeHAL classRule = new TestRules.InitializeHAL(); 
+    @Rule
+    public TestRules.ResetSimDeviceSimData testRule = new TestRules.ResetSimDeviceSimData(); 
+
+
     @Test
     public void testResponses() {
-        SparkPIDController mock = Mocks.createMock(SparkPIDController.class, new MockedSparkMaxPIDController(), new REVLibErrorAnswer());
+        SparkPIDController mock = Mocks.createMock(SparkPIDController.class, new MockedSparkMaxPIDController(1), 
+            new REVLibErrorAnswer(), CANPIDController.class, SparkMaxPIDController.class);
         assertSlotValueUpdate(mock::setP, mock::setP, mock::getP, mock::getP);
         assertSlotValueUpdate(mock::setI, mock::setI, mock::getI, mock::getI);
         assertSlotValueUpdate(mock::setD, mock::setD, mock::getD, mock::getD);
+
+
+        SimDeviceSim pidControllerSim = new SimDeviceSim("SparkPIDController", 1);
+        SimDouble velocitySim = pidControllerSim.getDouble("velocity");
+        SimEnum controlTypeSim = pidControllerSim.getEnum("controlType");
+        SimDouble arbFFSim = pidControllerSim.getDouble("arbFF");
+        SimEnum arbFFUnitsSim = pidControllerSim.getEnum("arbFFUnits");
+
+        assertThat(velocitySim.get(), is(0.0));
+        assertThat(controlTypeSim.get(), is(ControlType.kDutyCycle.ordinal()));
+        assertThat(arbFFSim.get(), is(0.0));
+        assertThat(arbFFUnitsSim.get(), is(ArbFFUnits.kVoltage.ordinal()));
+
+        assertEquals(REVLibError.kOk, mock.setReference(0.5, ControlType.kVelocity, 1, 0.25, ArbFFUnits.kPercentOut));
+        assertThat(velocitySim.get(), is(0.5));
+        assertThat(controlTypeSim.get(), is(ControlType.kVelocity.ordinal()));
+        assertThat(arbFFSim.get(), is(0.25));
+        assertThat(arbFFUnitsSim.get(), is(ArbFFUnits.kPercentOut.ordinal()));
     }
 
     private void assertSlotValueUpdate(Function<Double, REVLibError> setFunc, BiFunction<Double, Integer, REVLibError> slotSetFunc, Supplier<Double> getFunc, Function<Integer, Double> slotGetFunc) {
