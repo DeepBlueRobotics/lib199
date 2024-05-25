68bc5f1e0f37b32e3800c3542c0afceba77907c2
Dean Brettle <dean@brettle.com>
2024-05-24 18:36:30 -0700

    Fix intermittent test failure caused by async periodic methods left running across tests.

--- src/test/java/org/carlmontrobotics/lib199/testUtils/TestRules.java
+++ src/test/java/org/carlmontrobotics/lib199/testUtils/TestRules.java
@@ -1,5 +1,6 @@
 package org.carlmontrobotics.lib199.testUtils;
 
+import org.carlmontrobotics.lib199.Lib199Subsystem;
 import org.junit.rules.TestRule;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
@@ -30,8 +31,13 @@ public class TestRules {
             return new Statement(){
                 @Override
                 public void evaluate() throws Throwable {
+                    // Ensure there are no async periodic things running before
+                    // we reset the SimDeviceData so that they don't try to
+                    // touch devices/data after they have been removed.
+                    Lib199Subsystem.unregisterAllAsync();
                     SimDeviceSim.resetData();
                     base.evaluate();
+                    Lib199Subsystem.unregisterAllAsync();
                     SimDeviceSim.resetData();
                 }
             };
