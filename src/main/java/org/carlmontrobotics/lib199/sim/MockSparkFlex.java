c52cbd7f4bd80bf0d602f10b044a355f9da7a1c0
FriedLongJohns <81837862+FriedLongJohns@users.noreply.github.com>
2025-01-12 20:25:16 -0800

    that wasn't supposed to be commented out!

--- src/main/java/org/carlmontrobotics/lib199/sim/MockSparkFlex.java
+++ src/main/java/org/carlmontrobotics/lib199/sim/MockSparkFlex.java
@@ -1,18 +1,18 @@
-// package org.carlmontrobotics.lib199.sim;
+package org.carlmontrobotics.lib199.sim;
 
-// import org.carlmontrobotics.lib199.DummySparkMaxAnswer;
-// import org.carlmontrobotics.lib199.Mocks;
+import org.carlmontrobotics.lib199.DummySparkMaxAnswer;
+import org.carlmontrobotics.lib199.Mocks;
 
-// import com.revrobotics.spark.SparkLowLevel.MotorType;
-// import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkFlex;
 
-// public class MockSparkFlex extends MockSparkBase {
+public class MockSparkFlex extends MockSparkBase {
 
-//     public MockSparkFlex(int port, MotorType type) {
-//         super(port, type, "CANSparkFlex", 7168);
-//     }
+    public MockSparkFlex(int port, MotorType type) {
+        super(port, type, "CANSparkFlex", 7168);
+    }
 
-//     public static SparkFlex createMockSparkFlex(int portPWM, MotorType type) {
-//         return Mocks.createMock(SparkFlex.class, new MockSparkFlex(portPWM, type), new DummySparkMaxAnswer());
-//     }
-// }
+    public static SparkFlex createMockSparkFlex(int portPWM, MotorType type) {
+        return Mocks.createMock(SparkFlex.class, new MockSparkFlex(portPWM, type), new DummySparkMaxAnswer());
+    }
+}
