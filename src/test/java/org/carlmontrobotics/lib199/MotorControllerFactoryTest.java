caab1c51356836b84843bae024246ca9bea0cbd5
Tim <73599525+timtogan@users.noreply.github.com>
2026-02-05 11:19:56 -0800

    added tests for MotorControllerFactory

--- src/test/java/org/carlmontrobotics/lib199/MotorControllerFactoryTest.java
+++ src/test/java/org/carlmontrobotics/lib199/MotorControllerFactoryTest.java
@@ -20,7 +20,20 @@ public class MotorControllerFactoryTest extends ErrStreamTest {
     public void testCreateNoErrors() throws Exception {
         // Call close to free PWM ports
         ((AutoCloseable)MotorControllerFactory.createTalon(0)).close();
+
         MotorControllerFactory.createSparkMax(2, MotorConfig.NEO);
+        MotorControllerFactory.createSparkMax(3, MotorConfig.NEO, MotorControllerFactory.sparkConfig(MotorConfig.NEO));
+        
+        MotorControllerFactory.createSparkFlex(10);
+        MotorControllerFactory.createSparkFlex(10, MotorConfig.NEO_VORTEX, MotorControllerFactory.sparkConfig(MotorConfig.NEO_VORTEX));
+
+        MotorControllerFactory.createSpark(20, MotorConfig.NEO);
+        MotorControllerFactory.createSpark(21, MotorConfig.NEO_2);
+        MotorControllerFactory.createSpark(22, MotorConfig.NEO_VORTEX);
+        MotorControllerFactory.createSpark(23, MotorConfig.NEO, MotorControllerFactory.sparkConfig(MotorConfig.NEO_VORTEX));
+        MotorControllerFactory.createSpark(24, MotorConfig.NEO_2, MotorControllerFactory.sparkConfig(MotorConfig.NEO_VORTEX));
+        MotorControllerFactory.createSpark(25, MotorConfig.NEO_VORTEX, MotorControllerFactory.sparkConfig(MotorConfig.NEO_VORTEX));
+        
         assertEquals(0, errStream.toByteArray().length);
     }
 
